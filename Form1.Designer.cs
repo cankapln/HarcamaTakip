7d0c233137a79358a20edb33f12c15b0c2a94413
cankapln <cankaplan.php@gmail.com>
2025-07-10 03:08:36 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace HarcamaTakip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.txtAciklama = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbKategori = new System.Windows.Forms.ComboBox();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.dgvHarcamalar = new System.Windows.Forms.DataGridView();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHarcamalar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tutar :";
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(155, 64);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 22);
+            this.txtTutar.TabIndex = 1;
+            // 
+            // txtAciklama
+            // 
+            this.txtAciklama.Location = new System.Drawing.Point(155, 112);
+            this.txtAciklama.Name = "txtAciklama";
+            this.txtAciklama.Size = new System.Drawing.Size(100, 22);
+            this.txtAciklama.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Açıklama :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Kategori : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(75, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Tarih";
+            // 
+            // cmbKategori
+            // 
+            this.cmbKategori.FormattingEnabled = true;
+            this.cmbKategori.Location = new System.Drawing.Point(155, 153);
+            this.cmbKategori.Name = "cmbKategori";
+            this.cmbKategori.Size = new System.Drawing.Size(121, 24);
+            this.cmbKategori.TabIndex = 6;
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Location = new System.Drawing.Point(155, 191);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(200, 22);
+            this.dtpTarih.TabIndex = 7;
+            // 
+            // dgvHarcamalar
+            // 
+            this.dgvHarcamalar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHarcamalar.Location = new System.Drawing.Point(447, 28);
+            this.dgvHarcamalar.Name = "dgvHarcamalar";
+            this.dgvHarcamalar.RowHeadersWidth = 51;
+            this.dgvHarcamalar.RowTemplate.Height = 24;
+            this.dgvHarcamalar.Size = new System.Drawing.Size(304, 186);
+            this.dgvHarcamalar.TabIndex = 8;
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(155, 228);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 9;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Location = new System.Drawing.Point(537, 237);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(89, 23);
+            this.refresh.TabIndex = 10;
+            this.refresh.Text = "Güncelle";
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dgvHarcamalar);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.cmbKategori);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAciklama);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHarcamalar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.TextBox txtAciklama;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbKategori;
+        private System.Windows.Forms.DateTimePicker dtpTarih;
+        private System.Windows.Forms.DataGridView dgvHarcamalar;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button refresh;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace HarcamaTakip
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection conn = new MySqlConnection("server=localhost;database=harcamatakip;user=root;pwd=''");
+        MySqlCommand cmd = new MySqlCommand();
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+
+            VerileriListele();
+
+            MySqlDataReader dr;
+            conn.Open();
+            string qry = string.Format("select kategori_adi from kategoriler");
+            cmd.Connection = conn;
+            cmd.CommandText = qry;
+            dr = cmd.ExecuteReader();
+            while (dr.Read()) {
+                cmbKategori.Items.Add(dr["kategori_adi"].ToString());
+            }
+            conn.Close();
+            cmd.Dispose();
+
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (cmbKategori.SelectedIndex == -1 || string.IsNullOrWhiteSpace(txtTutar.Text) || string.IsNullOrWhiteSpace(txtAciklama.Text))
+            {
+                MessageBox.Show("Lütfen boş alan bırakmayın.");
+                return;
+            }
+
+            if (!decimal.TryParse(txtTutar.Text, out decimal amount))
+            {
+                MessageBox.Show("Geçerli bir tutar giriniz (örn: 123.45)");
+                return;
+            }
+
+            string exp = txtAciklama.Text.Replace("'", "''");
+            string category = cmbKategori.SelectedItem.ToString().Replace("'", "''");
+            string tarih = dtpTarih.Value.ToString("yyyy-MM-dd");
+
+            string qry = string.Format(
+                "INSERT INTO harcamalar (tutar, aciklama, kategori, tarih) VALUES ({0}, '{1}', '{2}', '{3}')",
+                amount, exp, category, tarih
+            );
+
+            try
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand(qry, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Kayıt başarıyla eklendi.");
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void VerileriListele()
+        {
+            try
+            {
+                conn.Open();
+                string sorgu = "SELECT id, tutar, aciklama, kategori, tarih FROM harcamalar ORDER BY tarih DESC";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(sorgu, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dgvHarcamalar.DataSource = dt;
+
+                
+                dgvHarcamalar.Columns["id"].HeaderText = "ID";
+                dgvHarcamalar.Columns["tutar"].HeaderText = "Tutar";
+                dgvHarcamalar.Columns["aciklama"].HeaderText = "Açıklama";
+                dgvHarcamalar.Columns["kategori"].HeaderText = "Kategori";
+                dgvHarcamalar.Columns["tarih"].HeaderText = "Tarih";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veriler yüklenirken hata: " + ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            VerileriListele();
+        }
+    }
+}
